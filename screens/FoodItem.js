1ab418952221c5bdd67b728748a8e09973d69a57
oykudanisik <79934309+oykudanisik@users.noreply.github.com>
2022-12-25 00:21:37 +0300

    local storage SOLVED

--- screens/FoodItem.js
+++ screens/FoodItem.js
@@ -169,8 +169,8 @@ const FoodItem = ({ route, navigation }) => {
                             }}
                             onPress={
                                 async () => {
-                                let x = await AsyncStorage.getItem("item");
-                                if(x == null){
+                                let items = await AsyncStorage.getItem("item");
+                                if(items == null){
                                     let array = [];
                                     route.params.item['count'] = 1;
                                     array.push(route.params.item);
@@ -179,18 +179,22 @@ const FoodItem = ({ route, navigation }) => {
                                         JSON.stringify(array),
                                     );
                                 } else{
-                                    x = await JSON.parse(x);
-                                    if (x.filter(e => e.id === route.params.item.id).length > 0) {
-                                        route.params.item.count = route.params.item.count+1;
-                                        console.log(route.params.item.count)
-                                    } else{
-                                        route.params.item['count'] = 1;
-                                        x.push(route.params.item);
+                                    let cartItems = await JSON.parse(items);
+                                    var found = false;
+                                    for(var i = 0; i<cartItems.length && !found;i++) {
+                                        if(cartItems[i].id === route.params.item.id){
+                                            cartItems[i].count ++;
+                                            found = true;
+                                        } 
+
                                     }
-                                    console.log("x",x);
+                                    if(!found){
+                                        route.params.item['count'] = 1;
+                                        cartItems.push(route.params.item);
+                                    }
                                     await AsyncStorage.setItem(
                                         "item", 
-                                        JSON.stringify(x),
+                                        JSON.stringify(cartItems),
                                     );
                                 }
                                 await navigation.goBack();
